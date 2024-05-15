d2ba97432fdf6bb4cd692df079eca607f6587c89
Andrew Le <andrew.le.careers@gmail.com>
2024-05-14 21:04:39 -0500

    Completed Exercise for Variable and Data Type

--- java/module-1/02_Variables_and_Datatypes/exercise/src/main/java/com/techelevator/Exercises.java
+++ java/module-1/02_Variables_and_Datatypes/exercise/src/main/java/com/techelevator/Exercises.java
@@ -47,48 +47,48 @@ public class Exercises {
         3. 3 raccoons are playing in the woods. 2 go home to eat dinner. How
         many raccoons are left in the woods?
         */
-
+		int raccoons = 1;
 
         /* Exercise 4
         4. There are 5 flowers and 3 bees. How many less bees than flowers?
         */
 
-
+		int bee = 2;
         /* Exercise 5
         5. 1 lonely pigeon was eating breadcrumbs. Another pigeon came to eat
         breadcrumbs, too. How many pigeons are eating breadcrumbs now?
         */
 
-
+		int pigeons = 2;
         /* Exercise 6
         6. 3 owls were sitting on the fence. 2 more owls joined them. How many
         owls are on the fence now?
         */
-
+		int owls =5;
 
         /* Exercise 7
         7. 2 beavers were working on their home. 1 went for a swim. How many
         beavers are still working on their home?
         */
-
+		int beaver =1;
 
         /* Exercise 8
         8. 2 toucans are sitting on a tree limb. 1 more toucan joins them. How
         many toucans in all?
         */
-
+		int toucans =3;
 
         /* Exercise 9
         9. There are 4 squirrels in a tree with 2 nuts. How many more squirrels
         are there than nuts?
         */
-
+		int squirrels =2;
 
         /* Exercise 10
         10. Mrs. Hilt found a quarter, 1 dime, and 2 nickels. How much money did
         she find?
         */
-
+		double total = .45;
 
         /* Exercise 11
         11. Mrs. Hilt's favorite first grade classes are baking muffins. Mrs. Brier's
@@ -97,178 +97,178 @@ public class Exercises {
         grade bake in all?
         */
 
-
+		int totalMuffins = 18+20+17;
         /* Exercise 12
         12. Mrs. Hilt bought a yoyo for 24 cents and a whistle for 14 cents. How
         much did she spend in all for the two toys?
         */
 
-
+		double totalCost = .24+.14;
         /* Exercise 13
         13. Mrs. Hilt made 5 Rice Krispies Treats. She used 8 large marshmallows
         and 10 mini marshmallows.How many marshmallows did she use
         altogether?
         */
 
-
+		int marshmallows = (8+10);
         /* Exercise 14
         14. At Mrs. Hilt's house, there was 29 inches of snow, and Brecknock
         Elementary School received 17 inches of snow. How much more snow
         did Mrs. Hilt's house have?
         */
 
-
+		int snowDifference = 29-17;
         /* Exercise 15
         15. Mrs. Hilt has $10. She spends $3 on a toy truck and $2.50 on a pencil
         case. How much money does she have left?
         */
 
-
+		double moneyLeft = 10-(3+2.5);
         /* Exercise 16
         16. Josh had 16 marbles in his collection. He lost 7 marbles. How many
         marbles does he have now?
         */
 
-
+		int marbles = 16-7;
         /* Exercise 17
         17. Megan has 19 seashells. How many more seashells does she need to
         find to have 25 seashells in her collection?
         */
 
-
+		int seashells = 25-19;
         /* Exercise 18
         18. Brad has 17 balloons. 8 balloons are red and the rest are green. How
         many green balloons does Brad have?
         */
 
-
+		int greenBalloons = 17-8;
         /* Exercise 19
         19. There are 38 books on the shelf. Marta put 10 more books on the shelf.
         How many books are on the shelf now?
         */
 
-
+		int books = 48;
         /* Exercise 20
         20. A bee has 6 legs. How many legs do 8 bees have?
         */
-
-
-        /* Exercise 21
+		int legs = 48;
+		/* Exercise 21
         21. Mrs. Hilt bought an ice cream cone for 99 cents. How much would 2 ice
         cream cones cost?
         */
 
-
+		double costOfIceCream = 2*.99;
         /* Exercise 22
         22. Mrs. Hilt wants to make a border around her garden. She needs 125
         rocks to complete the border. She has 64 rocks. How many more rocks
         does she need to complete the border?
         */
 
-
+		int rocks = 125-64;
         /* Exercise 23
         23. Mrs. Hilt had 38 marbles. She lost 15 of them. How many marbles does
         she have left?
         */
 
-
+		int marblesLeft = 38-15;
         /* Exercise 24
         24. Mrs. Hilt and her sister drove to a concert 78 miles away. They drove 32
         miles and then stopped for gas. How many miles did they have left to drive?
         */
 
-
+		int miles = 78-32;
         /* Exercise 25
         25. Mrs. Hilt spent 1 hour and 30 minutes shoveling snow on Saturday
         morning and 45 minutes shoveling snow on Saturday afternoon. How
         much total time (in minutes) did she spend shoveling snow?
         */
 
-
+		int time = 90+45;
         /* Exercise 26
         26. Mrs. Hilt bought 6 hot dogs. Each hot dog cost 50 cents. How much
         money did she pay for all of the hot dogs?
         */
 
-
+		double costHotDogs = 6*.5;
         /* Exercise 27
         27. Mrs. Hilt has 50 cents. A pencil costs 7 cents. How many pencils can
         she buy with the money she has?
         */
 
-
+		int pencils = 50/7;
         /* Exercise 28
         28. Mrs. Hilt saw 33 butterflies. Some of the butterflies were red and others
         were orange. If 20 of the butterflies were orange, how many of them
         were red?
         */
-
+		int butterflies = 33-20;
 
         /* Exercise 29
         29. Kate gave the clerk $1.00. Her candy cost 54 cents. How much change
         should Kate get back?
         */
 
-
+		double change = 1-.54;
         /* Exercise 30
         30. Mark has 13 trees in his backyard. If he plants 12 more, how many trees
         will he have?
         */
 
-
+		int tree = 25;
         /* Exercise 31
         31. Joy will see her grandma in two days. How many hours until she sees
         her?
         */
 
-
+		int hours = 2*24;
         /* Exercise 32
         32. Kim has 4 cousins. She wants to give each one 5 pieces of gum. How
         much gum will she need?
         */
 
-
+		int gum = 20;
         /* Exercise 33
         33. Dan has $3.00. He bought a candy bar for $1.00. How much money is
         left?
         */
 
-
+		double cost = 2.00;
         /* Exercise 34
         34. 5 boats are in the lake. Each boat has 3 people. How many people are
         on boats in the lake?
         */
 
-
+		int boats = 15;
         /* Exercise 35
         35. Ellen had 380 legos, but she lost 57 of them. How many legos does she
         have now?
         */
 
-
+		int legos = 380-57;
         /* Exercise 36
         36. Arthur baked 35 muffins. How many more muffins does Arthur have to
         bake to have 83 muffins?
         */
 
-
+		int muffins = 83-35;
         /* Exercise 37
         37. Willy has 1400 crayons. Lucy has 290 crayons. How many more
         crayons does Willy have then Lucy?
         */
 
-
+		int crayons = 1400 - 290;
         /* Exercise 38
         38. There are 10 stickers on a page. If you have 22 pages of stickers, how
         many stickers do you have?
         */
 
-
+		int stickers = 10*22;
         /* Exercise 39
         39. There are 100 cupcakes for 8 children to share. How much will each
         person get if they share the cupcakes equally?
         */
 
+		double cupcakesEach = 100/8.0;
 
         /* Exercise 40
         40. She made 47 gingerbread cookies which she will distribute equally in
@@ -276,20 +276,20 @@ public class Exercises {
         cookies will not be placed in a jar?
         */
 
-
+		int cookiesLeft = 47%6;
         /* Exercise 41
         41. She also prepared 59 croissants which she plans to give to her 8
         neighbors. If each neighbor received an equal number of croissants,
         how many will be left with Marian?
         */
 
-
+		int marianCroissants = 59%8;
         /* Exercise 42
         42. Marian also baked oatmeal cookies for her classmates. If she can
         place 12 cookies on a tray at a time, how many trays will she need to
         prepare 276 oatmeal cookies at a time?
         */
-
+		int trays = 276/12;
 
         /* Exercise 43
         43. Marian’s friends were coming over that afternoon so she made 480
@@ -297,27 +297,27 @@ public class Exercises {
         servings of bite-sized pretzels was Marian able to prepare?
         */
 
-
+		int numberOfServings = 480/12;
         /* Exercise 44
         44. Lastly, she baked 53 lemon cupcakes for the children living in the city
         orphanage. If two lemon cupcakes were left at home, how many
         boxes with 3 lemon cupcakes each were given away?
         */
 
-
+		int lemonCupcakes = (53-2)/3;
         /* Exercise 45
         45. Susie's mom prepared 74 carrot sticks for breakfast. If the carrots
         were served equally to 12 people, how many carrot sticks were left
         uneaten?
         */
 
-
+		int carrotsLeft = 74%12;
         /* Exercise 46
         46. Susie and her sister gathered all 98 of their teddy bears and placed
         them on the shelves in their bedroom. If every shelf can carry a
         maximum of 7 teddy bears, how many shelves will be filled?
         */
-
+		int shelves = 98/7;
 
         /* Exercise 47
         47. Susie’s mother collected all family pictures and wanted to place all of
@@ -325,7 +325,7 @@ public class Exercises {
         albums will she need if there are 480 pictures?
         */
 
-
+		int albums = 480/20;
         /* Exercise 48
         48. Joe, Susie’s brother, collected all 94 trading cards scattered in his
         room and placed them in boxes. If a full box can hold a maximum of 8
@@ -333,27 +333,36 @@ public class Exercises {
         the unfilled box?
         */
 
-
+		int boxesFilled = 94/8;
+		int cardsRemaining = 94%8;
         /* Exercise 49
         49. The Milky Way galaxy contains 300 billion stars. The Andromeda galaxy
         contains 1 trillion stars. How many stars do the two galaxies contain combined?
         */
 
+		long milkyWay = (long) 300*1_000_000_000;
+		long andromeda = (long) 1000*1_000_000_000;
+		//System.out.println(milkyWay);
+		//System.out.println(andromeda);
+		long totalStars = milkyWay+andromeda;
 
         /* Exercise 50
         50. Cristina baked 17 croissants. If she planned to serve this equally to
         her seven guests, how many will each have?
         */
 
-
+		double split = 17/7.0;
 	    /* Exercise 51
 	    51. Bill and Jill are house painters. Bill can paint a standard room in 2.15 hours, while Jill averages
 	    1.90 hours. How long will it take the two painters working together to paint 5 standard rooms?
 	    Hint: Calculate the rate at which each painter can complete a room (rooms / hour), combine those rates, 
 	    and then divide the total number of rooms to be painted by the combined rate.
 	    */
-     
-
+     	double billPaintRate = 1/2.15;
+		double jillPaintRate = 1/1.9;
+		double timePaintRoom = 5/(billPaintRate+jillPaintRate);
+		//System.out.println("timepaint");
+		//System.out.println(timePaintRoom);
 	    /* Exercise 52
 	    52. Create and assign variables to hold a first name, last name, and middle initial. Using concatenation,
 		build an additional variable to hold the full name in the order of last name, first name, middle initial. The
@@ -361,13 +370,21 @@ public class Exercises {
 		with a period. Use "Grace", "Hopper, and "B" for the first name, last name, and middle initial.
 		Example: "John", "Smith, "D" —> "Smith, John D."
 	    */
+		String firstName = "Grace";
+		String lastName = "Hopper";
+		String middleInitial = "B";
 
+		String fullName = lastName + ", "+firstName+" "+middleInitial+".";
 
 	    /* Exercise 53
 	    53. The distance between New York and Chicago is 800 miles, and the train has already travelled 537 miles.
 	    What percentage of the trip as a whole number has been completed?
 	    */
-
+		double percentageDistanceCompleted = (537/800.0)*100;
+		int finalResult = (int)percentageDistanceCompleted;
+		/*double percentageDistanceCompleted = (double) (537/800.0);
+		System.out.println("hello");
+		System.out.println(percentageDistanceCompleted);*/
 	}
 
 }
--- java/module-1/02_Variables_and_Datatypes/lecture/src/main/java/com/techelevator/Lecture.java
+++ java/module-1/02_Variables_and_Datatypes/lecture/src/main/java/com/techelevator/Lecture.java
@@ -1,132 +1,158 @@
 package com.techelevator;
 
+import java.sql.SQLOutput;
+
 public class Lecture {
 
 	public static void main(String[] args) {
-		
+
+		/*double x = (double) 5/2;
+		System.out.println(x);*/
+		;
 		/*
 		1. Create a variable to hold an int and call it numberOfExercises.
 			Then set it to 26.
-		*/
+			int numberOfExercises = 26;
 
+		*/
+		int numberOfExercises = 26;
+		System.out.println(numberOfExercises);
 		//System.out.println(numberOfExercises);
 
 		/*
 		2. Create a variable to hold a double and call it half.
 			Set it to 0.5.
 		*/
-
-		//System.out.println(half);
+		double half = 0.5;
+		System.out.println(half);
 
 		/*
 		3. Create a variable to hold a String and call it name.
 			Set it to "TechElevator".
 		*/
-
-		//System.out.println(name);
+		String name ="TechElevator";
+		System.out.println(name);
 
 		/*
 		4. Create a variable called seasonsOfFirefly and set it to 1.
 		*/
-
-		//System.out.println(seasonsOfFirefly);
+		int seasonsOfFirefly = 1;
+		System.out.println(seasonsOfFirefly);
 
 		/*
 		5. Create a variable called myFavoriteLanguage and set it to "Java".
 		*/
-
-		//System.out.println(myFavoriteLanguage);
+		String myFavoriteLanguage = "Java";
+		System.out.println(myFavoriteLanguage);
 
 		/*
 		6. Create a *constant* called PI and set it to 3.14159.
 		*/
-
-		//System.out.println(PI);
+		final double PI = 3.14159;
+		System.out.println(PI);
 
 		/*
 		7. Create and set a variable that holds your name.
 		*/
-
+		String myName = "Andrew";
 		/*
 		8. Create and set a variable that holds the number of buttons on your mouse.
 		*/
-
+		int numberOfButtonsOnMouse = 0;
+		System.out.println(numberOfButtonsOnMouse);
 		/*
 		9. Create and set a variable that holds the percentage of battery left on
 		your phone.
 		*/
-
+		int batteryLifePercentage = 94;
+		System.out.println(batteryLifePercentage+"%");
 		/*
 		10. Create an int variable that holds the difference between 121 and 27.
 		*/
 
+		int difference = 121-27;
+		System.out.println(difference);
 		/*
 		11. Create a double that holds the addition of 12.3 and 32.1.
 		*/
 
+		double addition = 12.3+32.1;
 		/*
 		12. Create a String that holds your full name.
 		*/
+		String fullName;
+		String firstName = "Andrew";
+		String lastName = "Le";
+
+		fullName = firstName +" "+ lastName;
+		System.out.println(fullName);
 
 		/*
 		13. Create a String that holds the word "Hello, " concatenated onto your
 		name from above.
 		*/
-
+		String hello = "Hello";
+		System.out.println(hello+" "+fullName);
 		/*
 		14. Add " Esquire" onto the end of your full name and save it back to
 		the same variable.
 		*/
-
+		fullName = fullName + " Esquire";
+		System.out.println("New name is "+fullName);
 		/*
 		15. Now do the same as exercise 14, but use the += operator.
 		*/
-
+		fullName = firstName + " "+lastName;
+		fullName += " Esquire";
+		System.out.println(fullName);
 		/*
 		16. Create a variable to hold "Saw" and add a 2 onto the end of it.
 		*/
-
+		String item = "Saw";
+		item = item + 2; //item += 2
+		System.out.println(item);
 		/*
 		17. Add a 0 onto the end of the variable from exercise 16.
 		*/
-
+		item += 0;
+		System.out.println(item);
 		/*
 		18. What is 4.4 divided by 2.2?
 		*/
-
+		System.out.println(4.4/2.2);
 		/*
 		19. What is 5.4 divided by 2?
 		*/
-
+		System.out.println(5.4/2);
 		/*
 		20. What is 5 divided by 2?
 		*/
-
+		System.out.println(5/2);
 		/*
 		21. What is 5.0 divided by 2?
 		*/
-
+		System.out.println(5.0/2);
 		/*
 		22. What is 66.6 divided by 100? Is the answer you get right or wrong?
 		*/
-
+		System.out.println(66.6/100);
 		/*
 		23. If I divide 5 by 2, what's my remainder?
 		*/
-
+		System.out.println(5%2);
 		/*
 		24. What is 1,000,000,000 * 3?
 		*/
-
+		System.out.println(1_000_000_000*3);
 		/*
 		25. Create a variable that holds a boolean called isDoneWithExercises and
 		set it to false.
 		*/
-
+		boolean isDoneWithExercises = false;
 		/*
 		26. Now set isDoneWithExercise to true.
 		*/
-		
+		isDoneWithExercises = true;
 	}
 
 }
