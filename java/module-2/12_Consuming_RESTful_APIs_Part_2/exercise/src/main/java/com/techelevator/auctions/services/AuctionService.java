7045710099470122e1622897867bebe864ee2432
Andrew Le <andrew.le.careers@gmail.com>
2024-06-26 18:17:45 -0500

    Completed HW 2 -12

--- java/module-2/12_Consuming_RESTful_APIs_Part_2/exercise/src/main/java/com/techelevator/auctions/services/AuctionService.java
+++ java/module-2/12_Consuming_RESTful_APIs_Part_2/exercise/src/main/java/com/techelevator/auctions/services/AuctionService.java
@@ -1,9 +1,7 @@
 package com.techelevator.auctions.services;
 
 import com.techelevator.util.BasicLogger;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.http.*;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
@@ -18,17 +16,61 @@ public class AuctionService {
 
     public Auction add(Auction newAuction) {
         // place code here
-        return null;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Auction> requestEntity = new HttpEntity<>(newAuction, headers);
+
+        try{
+            ResponseEntity<Auction> response = restTemplate.exchange(API_BASE_URL, HttpMethod.POST,requestEntity, Auction.class);
+            return response.getBody();
+
+        } catch(Exception e){
+            System.err.println("Error during POST request: "+e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+
+        //return null;
     }
 
     public boolean update(Auction updatedAuction) {
         // place code here
-        return false;
+        boolean update = false;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Auction> requestEntity = new HttpEntity<>(updatedAuction, headers);
+
+        try{
+            restTemplate.put(API_BASE_URL+"/"+updatedAuction.getId(),requestEntity);
+            return update = true;
+        }catch(Exception e){
+            System.err.println();
+            e.printStackTrace();
+            return update;
+        }
+
+
+
+
+
+        //return false;
     }
 
     public boolean delete(int auctionId) {
         // place code here
-        return false;
+        boolean isDeleted = false;
+        try{
+            restTemplate.delete(API_BASE_URL+"/"+auctionId);
+            return isDeleted = true;
+        }catch(Exception e){
+            System.err.println();
+            return isDeleted;
+        }
+
+        //return false;
     }
 
     public Auction[] getAllAuctions() {
