334ddb3185f48c686a86a85dc478be9327a5f3a8
Andrew Le <andrew.le.careers@gmail.com>
2024-06-27 08:39:49 -0500

    Progress for HW 2-13

--- java/module-2/13_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ java/module-2/13_Server_Side_APIs_Part_1/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -2,16 +2,48 @@ package com.techelevator.auctions.controller;
 
 import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.dao.MemoryAuctionDao;
+import com.techelevator.auctions.model.Auction;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/auctions")
+
 public class AuctionController {
 
     private AuctionDao auctionDao;
+
 
     public AuctionController() {
         this.auctionDao = new MemoryAuctionDao();
     }
 
+    @RequestMapping(path="/auctions", method = RequestMethod.GET)
+    public List<Auction> list(){
+
+        List <Auction> auctions = auctionDao.getAuctions();
+        return auctions;
+    }
+
+    /*@GetMapping
+    public Auction get(@RequestParm Integer id){
+
+        try{
+            return auctionDao.getAuctionById(id);
+        } catch(Exception e){
+            return null;
+        }
+
+        //return null;
+    }*/
+
+
+
+    /*@Override
+    public List<Auction> getAuctions() {
+        return auctions;
+    }*/
 }
