15b68ac42ff0cfa61aea9b073492d8dd8954a181
Andrew Le <andrew.le.careers@gmail.com>
2024-06-30 20:23:20 -0500

    Finished HW 2-14

--- java/module-2/14_Server_Side_APIs_Part_2/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
+++ java/module-2/14_Server_Side_APIs_Part_2/exercise/src/main/java/com/techelevator/auctions/controller/AuctionController.java
@@ -2,6 +2,7 @@ package com.techelevator.auctions.controller;
 
 import java.util.List;
 
+import com.techelevator.auctions.exception.DaoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,10 +16,11 @@ import com.techelevator.auctions.dao.AuctionDao;
 import com.techelevator.auctions.model.Auction;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/auctions")
 public class AuctionController {
-
     private AuctionDao auctionDao;
 
     public AuctionController(AuctionDao auctionDao) {
@@ -48,10 +50,34 @@ public class AuctionController {
         }
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @RequestMapping(path = "", method = RequestMethod.POST)
-    public Auction create(@RequestBody Auction auction) {
+    public Auction create(@Valid @RequestBody Auction auction) {
         return auctionDao.createAuction(auction);
+
     }
+    @ResponseStatus(HttpStatus.OK)
+    @RequestMapping(path= "{id}", method = RequestMethod.PUT)
+    public Auction update(@PathVariable int id, @Valid @RequestBody Auction auction){
+
+            // Ensure the ID in the path matches the ID in the request body
+            if (auction.getId() != id) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "ID in path does not match ID in auction");
+            }
+            return auctionDao.updateAuction(auction);
+
+
+
+    }
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @RequestMapping(path = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable int id, Auction auction){
+        if(auction.getId()==id){
+            auctionDao.deleteAuctionById(id);
+        }
+    }
+
+
 
 
 }
